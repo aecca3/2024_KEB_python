4e291b112ae55c5afd82d1d8a1e6af27c3a02186
aecca3 <aecca3@naver.com>
2024-01-18 14:12:21 +0900

    v4.7 closure day05

--- day05.py
+++ day05.py
@@ -1,15 +1,18 @@
-def squares(*n) -> list:
-    """
-    넘겨 받은 수치 데이터들의 거듭제곱 값을 리스트에 담아서 리턴
-    :param n: tuple
-    :return: list
-    """
-    return [pow(i, 2) for i in n]
-    #return n * n
+#closure 호출하는 함수에 있는 내부함수의 주소를 받는다/ 바깥쪽 함수의 매개변수 값도 가지고 있는 상태
+def out_func(nout):
+    def inner_func():
+        return nout * nout
+    return inner_func
 
-def run_function(f, *number) -> list:
-    return f(*number)
+x = out_func(9)
+print(type(x))
+print(x)
+print(x())
 
-
-print(squares(7, 5, 2))
-print(run_function(squares, 9, 10))
+#innerfunction
+# def out_func(nout):
+#     def inner_func(nin):
+#         return nin * nin
+#     return inner_func(nout)
+#
+# print(out_func(5))
