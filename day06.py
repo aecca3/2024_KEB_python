ced387b633bb86c17355229edc1822c4fe490f81
aecca3 <aecca3@naver.com>
2024-01-19 13:44:58 +0900

    v6.0  day06

--- day06.py
+++ day06.py
@@ -1,16 +1,27 @@
-def desc(f): #desc:decorator->다른 함수가 필요하다 따라서 def sth나오는 것/호출은 something()이렇게 한다
-    def wrapper(): #wrapper: closure/ 독립된 공간 desc 매개변수를 기억하고있다 호출한다고 나오는 것이 아님 f값을 기억한다
-        print("study")
-        f()
-    #print("a")
-    return wrapper
+#class Pokemon(): #class 앞글자 대문자
+class Pokemon:
+    def __init__(self, name ):#self는 자동 매개변수 s도 가능/init 해당객체에 딱 한번 돌 수 있다(initialize)
+        self.name = name
+        print(f"{name} 포켓몬스터 생성")
+    def attack(self, target):
+        print(f"{self.name}이(가) {target.name}을(를) 공격!")
 
-@desc #이렇게 @붙이면 그냥 decoration 함수 나온다....
-def something():
-    print("do something")
+charizard = Pokemon("리자몽")
+pikachu = Pokemon("피카츄")
+squirtle = Pokemon("꼬부기")
+charizard.attack(squirtle)
+# print(pikachu.name)
+# print(squirtle.name)
 
-s = desc(something) #desc()이 안에 f 넣으면 s = desc(something)이렇게 선언?해줘야한다..
-s()
-
-
-#is prime number decorator 붙여보기
+# class Pokemon():
+#     pass
+# pikachu = Pokemon()
+# squirtle = Pokemon()
+# pikachu.name = "피카츄"
+# pikachu.nemesis = squirtle
+# print(pikachu.name)
+# # print(pikachu.nemesis.name)
+# # pikachu = Pokemon("피카츄")
+# pikachu.nemesis.name = "꼬부기"
+# print(pikachu.nemesis.name)
+# print(squirtle.name)
