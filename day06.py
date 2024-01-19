903ed118b5931831d094ee41a89c9b5d69f43dcb
aecca3 <aecca3@naver.com>
2024-01-19 14:43:19 +0900

    v6.1  day06

--- day06.py
+++ day06.py
@@ -1,27 +1,31 @@
-#class Pokemon(): #class 앞글자 대문자
-class Pokemon:
-    def __init__(self, name ):#self는 자동 매개변수 s도 가능/init 해당객체에 딱 한번 돌 수 있다(initialize)
+class Pokemon :
+    def __init__(self, name):
         self.name = name
-        print(f"{name} 포켓몬스터 생성")
     def attack(self, target):
-        print(f"{self.name}이(가) {target.name}을(를) 공격!")
+        print(f"{self.name}이(가) {target.name}을 공격!")
+class Pikachu(Pokemon): #Pikachu is a Pokemon 상속이 되는 것 부모를 보면 된다/
+    # #추상화된 언어(abstract class instance는 볼 수 없지만 분류나 상속의 목적으로... 이 예시에선 포켓몬)
+    def __init__(self, name, type):
+        super().__init__(name)
+        self.type = type
+    def attack(self, target):
+        print(f"{self.name}이(가) {target.name}을 {self.type} 공격!")
+    def electric_info(self) :
+        print("전기 계열의 공격을 합니다")
+class Squirtle(Pokemon) :
+    pass
 
-charizard = Pokemon("리자몽")
-pikachu = Pokemon("피카츄")
-squirtle = Pokemon("꼬부기")
-charizard.attack(squirtle)
-# print(pikachu.name)
-# print(squirtle.name)
+class Agumon(Pokemon) :
+    pass
 
-# class Pokemon():
-#     pass
-# pikachu = Pokemon()
-# squirtle = Pokemon()
-# pikachu.name = "피카츄"
-# pikachu.nemesis = squirtle
-# print(pikachu.name)
-# # print(pikachu.nemesis.name)
-# # pikachu = Pokemon("피카츄")
-# pikachu.nemesis.name = "꼬부기"
-# print(pikachu.nemesis.name)
-# print(squirtle.name)
+p1 = Pikachu("피카츄", "전기")
+p2 = Squirtle("꼬부기")
+p3 = Pokemon("아무개")
+p1.electric_info()
+#p3.electric_info()
+p1.attack(p2)
+p2.attack(p1)
+print(p1.name, p2.name)
+print(p1.name)
+print(issubclass(Pikachu, Pokemon))
+print(issubclass(Agumon, Pokemon))
